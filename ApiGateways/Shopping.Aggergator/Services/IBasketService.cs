2def47714ed8bbb51de58d70ab404ad80d52503e
DevThet <DevThet@hotmail.com>
2022-01-22 15:13:04 +0630

    ApiGateway and Aggregation

---
+++ ApiGateways/Shopping.Aggergator/Services/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using Shopping.Aggergator.Models;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggergator.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+    }
+}
---
+++ ApiGateways/Shopping.Aggergator/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using Shopping.Aggergator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggergator.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
